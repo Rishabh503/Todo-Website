afaf16645528a056f546eeab755b2bcdef255b5d
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-01-06 16:09:00 +0530

    fixed button issues

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,29 +1,34 @@
-import { useState } from "react"
-import { useTodo } from "../context/todocontext"
+import { useTodo } from "../context/todocontext";
 
-export const Item=({curTodo})=>{
-    console.log(curTodo.todo)
-    // const [todoMsg,setTodoMsg]=useState(curTodo.todo)
-    // console.log(todoMsg)
-    const {deleteTodo,toggleComplete}=useTodo()
-    const checker=curTodo.completed?"bg-green-300":"bg-white"
+export const Item = ({ curTodo }) => {
+  const { deleteTodo, toggleComplete } = useTodo();
 
-    return(
-        <div className={`bg-white ${checker} p-1 rounded-lg flex border justify-between` }>
-            <div className="text-3xl">
-            <input className="h-5 w-5 mr-2" type="checkbox" checked={curTodo.completed} onChange={()=>{toggleComplete(curTodo.id)}}>
-            </input>
-            {curTodo.todo}
-            </div>
-            <div>
-            <button onClick={()=>{deleteTodo(curTodo.id)}} className="bg-blue-400 ml-2 p-2">
-                delete 
-            </button>
-            <button className="bg-blue-400 ml-2 p-2">
-                edit
-            </button>
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div
+      className={`bg-white ${
+        curTodo.completed ? "bg-green-600" : "bg-yellow-700"
+      } p-2 rounded-lg flex border justify-between`}
+    >
+      <div className="flex items-center text-lg">
+        <input
+          className="h-5 w-5 mr-3"
+          type="checkbox"
+          checked={curTodo.completed}
+          onChange={() => toggleComplete(curTodo.id)} // Update the state here
+        />
+        {curTodo.todo}
+      </div>
+      <div>
+        <button
+          onClick={() => deleteTodo(curTodo.id)} // Handle deletion
+          className="bg-red-500 text-white px-3 py-1 rounded-lg mr-2"
+        >
+          Delete
+        </button>
+        <button className="bg-blue-500 text-white px-3 py-1 rounded-lg">
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+};
